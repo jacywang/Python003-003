f0df151d170d22cce77c162690c2551894881ac1
Jian Wang <jian.wang@fatiguescience.com>
2020-08-29 23:16:47 -0700

    Save to MySql

--- week02/assignment1/assignment2/pipelines.py
+++ week02/assignment1/assignment2/pipelines.py
@@ -6,20 +6,34 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
-from scrapy.exporters import CsvItemExporter
-
+import pymysql
+from scrapy.exceptions import DropItem
 
 class Assignment2Pipeline:
     def __init__(self):
-        self.file = open('./movies.csv', 'wb')
-        self.exporter = CsvItemExporter(self.file)
-        self.exporter.fields_to_export = ['name', 'movie_type', 'release_date']
-        self.exporter.start_exporting()
+      try:
+        self.connection = pymysql.connect(host='localhost',
+                                          user='user',
+                                          password='password',
+                                          db='db',
+                                          charset='utf8mb4',)
+      except Exception as e:
+        raise DropItem(f'Connection to MySQL failed {e}')
+      self.cursor = self.connection.cursor()
 
     def close_spider(self, spider):
-        self.exporter.finish_exporting()
-        self.file.close()
+      try:
+        self.cursor.close()
+        self.connection.commit()
+        self.connection.close()
+      except Exception as e:
+        raise DropItem(f'Failed to close {e}')
 
     def process_item(self, item, spider):
-        self.exporter.export_item(item)
-        return item
+      try:
+        sql = "INSERT INTO 'movies' ('name', 'type', 'release_date) VALUES (%s, %s, %s))"
+        self.cursor.execute(sql, tuple(item.values()))
+      except Exception as e:
+        self.connection.rollback()
+        print(f'Insert item failed {e}')
+      return item
